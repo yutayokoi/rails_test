bab991bd605186c9735f7ee46f9343575b7e60c4
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-20 04:19:56 +0000

    rayout modified

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -12,11 +12,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
 end
