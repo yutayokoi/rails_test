7a3f04fac5684f8af195cb33c0a3afcb5dd84acf
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-21 07:17:29 +0000

    remember user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,28 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   fixtures :all
 
-  # テストユーザーがログインしていればtrueを返す
+  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # 結合テスト内ではtrueを返す
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
