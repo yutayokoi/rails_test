838bdbf82c62fe8dd6b00ca1dd9e92671661cec7
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-21 05:40:44 +0000

    logged add

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
