e9dcd0fb2bc4b65cda0acee3e3d9a1607069a218
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-21 07:04:23 +0000

    session modified

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,4 +35,18 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーがログアウトする
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,9 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  # ユーザーログインを破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
