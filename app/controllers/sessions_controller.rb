69f2db03316772f83cf9311a45fb87b151ec9c95
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-21 05:01:51 +0000

    session def

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
