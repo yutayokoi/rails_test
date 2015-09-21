56fd1d68c4ca37d485e1ed0052f43fa5f4f19eaa
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-21 07:59:50 +0000

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, presence: true
 
   # 与えられた文字列のハッシュ値を返す
   def User.digest(string)
