37eb3e79282ee88bce0f372490c95aa2b121ad1b
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-22 01:25:02 +0000

    edit_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
