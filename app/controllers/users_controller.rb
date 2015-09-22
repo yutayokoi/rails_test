efc107cc27ab2d74da0a8d47b1e27363d30bd17d
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-22 02:37:59 +0000

    Revert "friendly routing"
    This reverts commit 40ea4bd7ab36781fd67c0aaf2d8d2c00937ddf4a.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,6 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
-        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
