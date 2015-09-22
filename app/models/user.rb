bff31b83580205ef3d723d91692cdeeb5dcf8fd5
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-22 05:56:54 +0000

    10.24

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,11 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーログインを破棄する
