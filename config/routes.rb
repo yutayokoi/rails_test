c86ec95262e065e3f639ce52655e84460ccd1c2d
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-22 06:36:44 +0000

    rails generate controller PasswordResets new edit --no-test-framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
