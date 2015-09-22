498a1962f31746a461aa9b6ae7ccf52072cde482
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-22 06:14:35 +0000

    10.33

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,17 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  # アカウントを有効にする
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     # メールアドレスをすべて小文字にする
