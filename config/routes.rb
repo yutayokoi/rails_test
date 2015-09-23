0b10903a5e620417588b3463e51ba3764e4a8d01
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-23 06:03:14 +0000

    12.15

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
