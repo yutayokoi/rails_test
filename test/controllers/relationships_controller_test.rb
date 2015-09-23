02ef25fc499572386a6e2b1f865d1aac62871381
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-23 06:34:50 +0000

    12.30

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      post :create
+    end
+    assert_redirected_to login_url
+  end
+
+  test "destroy should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      delete :destroy, id: relationships(:one)
+    end
+    assert_redirected_to login_url
+  end
 end
