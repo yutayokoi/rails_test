97eb439387d0942d96d819b8fb5d21b59c255c67
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-23 01:19:49 +0000

    11.1

--- db/migrate/20150923011832_create_microposts.rb
+++ db/migrate/20150923011832_create_microposts.rb
@@ -7,5 +7,6 @@ class CreateMicroposts < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_foreign_key :microposts, :users
+    add_index :microposts, [:user_id, :created_at]
   end
 end
