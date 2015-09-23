c918c88d819ce395d0b0535c82d0fd4c3eaff6c6
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-23 03:46:39 +0000

    11.61

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+    # アップロード画像のサイズを検証する
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
