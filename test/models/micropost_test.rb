bb5df2edb22909fc51dfeb1b5614e76129854dda
yuta_yokoi <dezirus.novajn.iojn@gmail.com>
2015-09-23 01:21:50 +0000

    11.2

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    # 次の行は慣習的に間違っている
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
